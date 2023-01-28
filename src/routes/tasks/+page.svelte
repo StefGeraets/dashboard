7c3a596abe6c8945c37573927c585ee59d863af0
StefGeraets <stef.geraets@gmail.com>
2023-01-28 11:22:46 +0100

    Basic todos schema

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,38 +1,56 @@
 <script lang="ts">
 	import { DotsVertical } from 'tabler-icons-svelte';
 
-	let tasks = [
-		{ title: 'Water plants', id: 0 },
-		{ title: 'Do Laundry', id: 0 },
-		{ title: 'Workout', id: 0 },
-		{ title: 'Meditate', id: 0 }
+	let todos = [
+		{ title: 'Water plants', id: 0, tags: [{ id: 0, name: 'Home', type: 'shore' }] },
+		{
+			title: 'Do Laundry',
+			id: 1,
+			repeat: true,
+			repeatAmount: 3,
+			repeatPeriod: 'Week',
+			repeatCheckins: [{ checkinDate: '28-01-2023' }, { checkinDate: '25-01-2023' }],
+			tags: [{ id: 0, name: 'Habit', type: 'habit' }]
+		},
+		{
+			title: 'Workout',
+			id: 2,
+			tags: [
+				{ id: 1, name: 'Schutterij', type: 'organisation' },
+				{ id: 1, name: 'Personal', type: 'personal' }
+			]
+		},
+		{ title: 'Meditate', id: 3, completed: true }
 	];
 </script>
 
 <main class="flex gap-6">
 	<div class="w-2/5 rounded-md bg-gray-800 px-4 py-3 shadow">
 		<h2 class="pb-3 text-lg uppercase tracking-wide text-gray-300">Habits</h2>
+		<div class="flex flex-col gap-4" />
+	</div>
+	<div class="w-2/5 rounded-md bg-gray-800 px-4 py-3 shadow">
+		<h2 class="pb-3 text-lg text-gray-300">Todo</h2>
 		<div class="flex flex-col gap-4">
-			{#each tasks as task}
+			{#each todos as todo}
 				<article class="w-full rounded-lg bg-gray-700 p-4 text-gray-200 shadow">
 					<div class="flex justify-between">
-						<h4 class="text-lg font-medium">{task.title}</h4>
+						<h4 class="text-lg font-medium">{todo.title}</h4>
 						<button
 							class="rounded p-1 text-gray-500 transition hover:bg-gray-800 hover:text-gray-300"
 							><DotsVertical size="20" /></button
 						>
 					</div>
 					<div class="flex gap-2">
-						<span class="rounded-lg bg-fuchsia-300 px-1 py-px text-xs font-bold text-fuchsia-700"
-							>home</span
-						>
+						{#each todo.tags || [] as tag}
+							<span class="rounded-lg bg-fuchsia-300 px-1 py-px text-xs font-bold text-fuchsia-700"
+								>{tag.name}</span
+							>
+						{/each}
 					</div>
 				</article>
 			{/each}
 		</div>
-	</div>
-	<div class="w-2/5 rounded-md bg-gray-800 px-4 py-3 shadow">
-		<h2 class="pb-3 text-lg text-gray-300">Todo</h2>
 	</div>
 	<div class="w-1/5 rounded-md bg-gray-800 px-4 py-3 shadow">
 		<h2 class="pb-3 text-lg text-gray-300">Completed</h2>
